167e2d28e1241537c1c6c603f09e95e2e7a6d7d8
Aicprog <aicprog@gmail.com>
2022-03-13 00:29:34 -0800

    Skeleton project created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<About />
+			<Experience />
+			<Skills />
+			<Projects />
+			<Testimonials />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
